b2534d8798791403f910b5a27cd6026c07e1cfa5
wambui-g <grayciekamau03@gmail.com>
2023-07-13 06:58:09 -0700

    2.1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     form sys import argv
 argc = len(argv)
 if argc < 2:
-    print("{} arguements:".format(argc - 1))
+    print("{} arguments:".format(argc - 1))
 else:
     if argc == 2:
-        print("{} arguement:".format(argc - 1))
+        print("{} argument:".format(argc - 1))
     else:
-        print("{} arguements:".format(argc - 1))
+        print("{} arguments:".format(argc - 1))
     for n in range(1, argc):
         print("{}: {}".format(n, argv[n]))
