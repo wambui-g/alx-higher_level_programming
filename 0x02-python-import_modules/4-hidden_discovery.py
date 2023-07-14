00f28d5e335eee74b72ad322b168aec3981428e5
wambui-g <grayciekamau03@gmail.com>
2023-07-13 23:00:12 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ import hidden_4
 if __name__ == "__main__":
     file = dir(hidden_4)
     length = len(file)
-    for i in range (0, length):
+    for i in range(0, length):
         if file[i][0:2] != "__":
             print(file[i])
