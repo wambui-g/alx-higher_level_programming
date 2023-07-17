7ab5b4c47faf605ada46af4555e793b6fdef1848
wambui-g <grayciekamau03@gmail.com>
2023-07-17 04:54:07 -0700

    4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(mylist)
+    cpy = my_list.copy()
+    if idx < 0:
+        return cpy
+    elif idx > n - 1:
+        return cpy
+    else:
+    cpy[idx] = element
+    return cpy
