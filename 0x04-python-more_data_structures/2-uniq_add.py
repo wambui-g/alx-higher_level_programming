1eaf7ba79139e63fb8804bfc6ee97f5c74d7cc9c
wambui-g <grayciekamau03@gmail.com>
2023-07-19 18:23:30 +0000

    4

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     new = set(my_list)
     sum = 0
     for x in new:
-        sum = sum +1
+        sum = sum + x
     return sum
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elemts(set_1, set_2):
-    common_set = set_1 & set_2
-    return common_set
+    common_set = set(set_1) & set(set_2)
+    return list(common_set)
