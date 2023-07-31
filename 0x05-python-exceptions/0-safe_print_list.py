f66eb2d47fd0f4924f2dafa53500dea0d69c01d7
wambui-g <grayciekamau03@gmail.com>
2023-07-31 02:33:11 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num_elements_printed = 0;
-    try:
-        for element in my_list:
-            if num_elements_printed < x:
-                print(element, end=' ')
-                num_elements_printed += 1
-    except:
-        pass
-
-    print()
+    for index in range(x):
+        try:
+            print(my_list[index], end='')
+            num_elements_printed += 1
+        except Exception as error:
+            break
+    print('')
     return num_elements_printed
