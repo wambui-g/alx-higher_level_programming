b73a5ea336bac2f2f669a734cfdda89139a64118
wambui-g <grayciekamau03@gmail.com>
2023-08-01 02:47:28 -0700

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,19 +5,19 @@
 class Square:
     """Square class
     Attributes:
-    __size (int): length
+        __size (int): length
     """
-    def __init__(self, size = 0)
-    """Initializes the class
-    Args:
-    size (int): length
-    Returns:
-    None
-    """
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    else:
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        """Initializes the class
+        Args:
+            size (int): length
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         else:
-            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
