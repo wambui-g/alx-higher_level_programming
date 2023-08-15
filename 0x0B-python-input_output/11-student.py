ec42af4a8d3d278eb15a3fed1b81752daa0ea0f7
wambui-g <grayciekamau03@gmail.com>
2023-08-15 17:35:17 +0000

    10.1

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """method that returns directory desc"""
         obj = self.__dict__.copy()
         if type(attrs) is list:
