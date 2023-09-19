673aa911b724254d09801ed4ba97d09ae68d18ba
wambui-g <grayciekamau03@gmail.com>
2023-09-19 05:38:58 -0700

    7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- inserts a new row in first_table
 -- id=89, name=Best School
 
-INSERT INTO 'first_table' ('id', 'name') VALUES(89, 'Best School');
+INSERT INTO first_table ('id', 'name') VALUES(89, 'Best School');
 SELECT COUNT(*) FROM first_table WHERE id=89;
