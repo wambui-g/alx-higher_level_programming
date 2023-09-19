e97ff91ddd01c3eefade56d3b6adce9d9d5c1dea
wambui-g <grayciekamau03@gmail.com>
2023-09-19 05:45:47 -0700

    all

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- computes the score avg of all records in second_table
 -- result col should be avg
 
-SELECT AVG(score) AS averge FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
